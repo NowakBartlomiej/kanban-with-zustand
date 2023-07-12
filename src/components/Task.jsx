79638fdf44a0600cf94d79e42a98615c1c1f681b
Bartlomiej <16bartlomiejnowak@gmail.com>
2023-07-12 21:26:47 +0200

    Implemented deleting tasks

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -2,17 +2,21 @@ import React from 'react'
 import './Task.css'
 import classNames from 'classnames'
 import { useStore } from '../store'
+import trashSvg from '../assets/trash-2.svg'
 
 const STATUS = "ONGOING"
 
 export const Task = ({title}) => {
   const task = useStore((store) => store.tasks.find(task => task.title === title))
+  const deleteTask = useStore((store) => store.deleteTask)
   
   return (
     <div className='task'>
         <div>{task.title}</div>
         <div className='bottomWrapper'>
-            <div></div>
+            <div>
+              <img src={trashSvg} onClick={() => deleteTask(task.title)}/>
+            </div>
             <div className={classNames("status", task.state)}>{task.state}</div>
         </div>
     </div>
