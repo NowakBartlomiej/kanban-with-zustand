805486dbf4231d3c0b76809f94b154e2913772d1
Bartlomiej <16bartlomiejnowak@gmail.com>
2023-07-12 21:44:21 +0200

    Implemented moving tasks

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -3,19 +3,38 @@ import "./Column.css"
 import { Task } from './Task'
 import { useStore } from '../store'
 import { shallow } from 'zustand/shallow'
+import classNames from 'classnames'
 
 export const Column = ({state}) => {
   const [text, setText] = useState("");
   const [open, setOpen] = useState(false);
+  const [drop, setDrop] = useState(false);
 
   const tasks = useStore((store) => 
     store.tasks.filter((task) => task.state === state),
   );
 
   const addTask = useStore((store) => store.addTask)
+  const setDraggedTask = useStore((store) => store.setDraggedTask)
+  const draggedTask = useStore((store) => store.draggedTask)
+  const moveTask = useStore((store) => store.moveTask)
   
   return (
-    <div className='column'>
+    <div className={classNames('column', {drop: drop})} 
+      onDragOver={e => {
+        setDrop(true)
+        e.preventDefault()
+      }}
+      onDragLeave={e => {
+        setDrop(false)
+        e.preventDefault()
+      }} 
+      onDrop={e => {
+        setDrop(false)
+        moveTask(draggedTask, state)
+        setDraggedTask(null);
+      }}
+      >
         <div className='titleWrapper'>
           <p>{state}</p>
           <button onClick={() => {
